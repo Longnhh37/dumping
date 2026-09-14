6d5e9cf88a1d72b03f23fa175b8e414135319948
Longnhh37 <Longnhh03@gmail.com>
2026-09-14 18:29:50 +0800

    new commit

---
+++ rust_atomic_and_locks/src/spinlock.rs
@@ -0,0 +1,11 @@
+use std::sync::atomic::AtomicBool;
+
+pub struct SpinLock {
+    locked: AtomicBool,
+}
+
+impl SpinLock {
+    pub const fn new() -> Self {
+        Self { locked: AtomicBool::new(false) }
+    }
+}
