81dbd0d8a9fdee5f7382827706b1fe0089fabeb9
Longnhh37 <Longnhh03@gmail.com>
2026-09-18 17:38:28 +0800

    add method for spinlock

--- rust_atomic_and_locks/src/spinlock.rs
+++ rust_atomic_and_locks/src/spinlock.rs
@@ -1,4 +1,5 @@
 use std::sync::atomic::AtomicBool;
+use std::sync::atomic::Ordering::*;
 
 pub struct SpinLock {
     locked: AtomicBool,
@@ -7,5 +8,15 @@ pub struct SpinLock {
 impl SpinLock {
     pub const fn new() -> Self {
         Self { locked: AtomicBool::new(false) }
+    }
+
+    pub fn lock(&self) {
+        while self.locked.swap(true, Acquire) {
+            std::hint::spin_loop();
+        }
+    }
+
+    pub fn unlock(&self) {
+        self.locked.store(false, Release);
     }
 }
